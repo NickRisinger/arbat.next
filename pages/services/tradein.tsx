89435c3b309277f4c67ec41ee66f189282011b4e
boozy044 <boozy044@gmail.com>
2023-06-01 18:09:16 +0300

    add hero component

--- pages/services/tradein.tsx
+++ pages/services/tradein.tsx
@@ -1,9 +1,10 @@
 import AppLayout from '~/layouts/AppLayout';
+import Hero from '~/components/services/Hero';
 
 const TradeInPage: NextPage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      ServicesPage
+    <main className="">
+      <Hero title="" description=""></Hero>
     </main>
   );
 };
